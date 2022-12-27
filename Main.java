e1c2c82af858065bd3aadd3ec13ba47e05a7e22a
Nothingrrrrrrksa <115745439+Nothingrrrrrrksa@users.noreply.github.com>
2022-12-27 17:59:58 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    
+        public static void main(String[] args) {
+    
+        int numero1 = 5;
+        int numero2 = 10;
+        int numero3 = 200;
+
+        sumatoria(numero1, numero2, numero3);
+     
+    }
+    
+    public static void sumatoria(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+}
+
+
